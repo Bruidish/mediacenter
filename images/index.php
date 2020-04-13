f9ea3b1fb4e545e02d9cdd4213f8016ca76d46bf
Michel Dumont <michel@dumont.io>
2020-04-13 21:39:08 +0200

    add missing dir

---
+++ images/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @version 1.0.0 - 2016-11-12 by <michel.dumont.io>
+ */
+
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+header("Location: ../");
+exit;
